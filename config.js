7451ac4883ee1ab328d158b53c5ad89326f06a91
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2019-02-28 12:52:26 -0800

    Pushing to heroku

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = process.env.DEBUG; 
-exports.ENVIRONMENT = 'production'; 
+exports.ENVIRONMENT = 'sandbox'; 
 exports.CALLBACK_URL = 'http://localhost:3001'; 
 exports.PUSH_TOPIC = '/topic/LogEntries';
 
-exports.CLIENT_ID = process.env.CLIENT_ID;
-exports.CLIENT_SECRET = process.env.CLIENT_SECRET;
-exports.USERNAME = process.env.USERNAME;
-exports.PASSWORD = process.env.PASSWORD; 
+exports.CLIENT_ID = '3MVG98dostKihXN5YKaqAhSPq24v0T1s16aDkghym3T.1OuMoeExPJupHwEWF5UyoUa2XBCenV6_87LjNKC8n';
+exports.CLIENT_SECRET = '8430B9145F67506ABB8F1008BE2889C1D3CDC0B882BF9A65AEF1354CA39E7121';
+exports.USERNAME = 'lucas.calegari@canadadrives.ca.se2lucas';
+exports.PASSWORD = 'dtt.170293'; 
