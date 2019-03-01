a97af217b4f9d082366371abf20cd400a538bd67
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2019-02-28 16:19:03 -0800

    test

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var sfdc = nforce.createConnection({
 // Configuration
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.bodyParser());
+// app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname + '/public')); 
