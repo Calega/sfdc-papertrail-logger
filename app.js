9d6b546c98462080a3347c18041544bd31548ade
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2019-02-28 16:10:55 -0800

    Use strict test

--- app.js
+++ app.js
@@ -25,22 +25,14 @@ var sfdc = nforce.createConnection({
 });
 
 // Configuration
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public')); 
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(__dirname + '/public')); 
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(express.errorHandler());
 
 // Routes
 app.get('/', routes.index);
